63e9db5a8cab1cfe3d4d8ef04a6a6269fbe8603d
Lemonish <patrikmihelson@gmail.com>
2025-01-16 20:29:52 +0200

    It seems a lot of boilerplate was generated while autogenerating all the necessary directories with npx. Used a one-liner bash script to remove all unnecessary files for these exercises and put a simple App.tsx instead.

---
+++ Laskin_navigoinnilla/App.tsx
@@ -0,0 +1,1 @@
+import React from 'react'; import { View, Text } from 'react-native'; export default function App() { return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Welcome to Laskin_navigoinnilla\!</Text></View>); }
