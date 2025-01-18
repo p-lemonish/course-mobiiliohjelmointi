9e320e893b0d631b51b76be4f985c74dfdaba480
Lemonish <patrikmihelson@gmail.com>
2025-01-18 16:43:03 +0200

    Numeron_arvaus valmis

--- Numeron_arvaus/app/index.tsx
+++ Numeron_arvaus/app/index.tsx
@@ -1,1 +1,59 @@
-import React from 'react'; import { View, Text } from 'react-native'; export default function App() { return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Welcome to Numeron_arvaus\!</Text></View>); }
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
+
+const randVal = Math.floor(Math.random() * 100) + 1;
+
+export default function App() {
+  const [guess, setGuess] = useState<number | null>(null);
+  const [num1, setNum1] = useState<string>("");
+  const [amountOfGuesses, setAmountOfGuesses] = useState<number>(1);
+
+  const handleGuess = () => {
+    const parsedNumber = parseInt(num1, 10);
+    setGuess(parsedNumber);
+    if (parsedNumber === randVal) {
+      Alert.alert(`You guessed the number in ${amountOfGuesses} guesses`);
+    } else {
+      setAmountOfGuesses((prev) => prev + 1);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      {guess !== null && (
+        <Text>
+          Your guess {guess} is {guess < randVal && <Text>too low</Text>}
+          {guess > randVal && <Text>too high</Text>}
+          {guess === randVal && <Text>correct!</Text>}
+        </Text>
+      )}
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={(val) => setNum1(val)}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Make guess" onPress={handleGuess} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "30%",
+    paddingTop: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "60%",
+    height: 40,
+    borderWidth: 1,
+  },
+});
