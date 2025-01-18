0fb6def8647f38a36a0c1908da25c3761b085885
Lemonish <patrikmihelson@gmail.com>
2025-01-18 18:25:17 +0200

    Laskin_historialla valmis

--- Laskin_historialla/app/index.tsx
+++ Laskin_historialla/app/index.tsx
@@ -1,1 +1,85 @@
-import React from 'react'; import { View, Text } from 'react-native'; export default function App() { return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Welcome to Laskin_historialla\!</Text></View>); }
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  FlatList,
+} from "react-native";
+export default function App() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState<number | null>(null);
+  const [history, setHistory] = useState<string[]>([]);
+
+  const renderItem = ({ item }: { item: string }) => {
+    return (
+      <View>
+        <Text>{item}</Text>
+      </View>
+    );
+  };
+
+  const handleSum = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+    const newHistoryItem = num1 + " + " + num2 + " = " + sum;
+    setHistory((prevHistory) => [...prevHistory, newHistoryItem]);
+  };
+
+  const handleDiff = () => {
+    const diff = parseFloat(num1) - parseFloat(num2);
+    setResult(diff);
+    const newHistoryItem = num1 + " - " + num2 + " = " + diff;
+    setHistory((prevHistory) => [...prevHistory, newHistoryItem]);
+  };
+
+  return (
+    <View style={styles.container}>
+      {result !== null && <Text>Result: {result}</Text>}
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={(text) => setNum1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={num2}
+        onChangeText={(text) => setNum2(text)}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={handleSum} />
+        <Button title="-" onPress={handleDiff} />
+      </View>
+
+      <FlatList
+        data={history}
+        renderItem={renderItem}
+        ListHeaderComponent={<Text>History</Text>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "30%",
+    paddingTop: 10,
+    marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "60%",
+    height: 40,
+    borderWidth: 1,
+  },
+});
